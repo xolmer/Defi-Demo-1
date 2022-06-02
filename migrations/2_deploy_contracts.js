38e8766c0a2e64fe304b0b9c1221a79823801994
Xolmer <xolmer@Xolmers-MacBook-Pro.local>
2022-06-02 16:50:31 +0200

    defi setup

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-// const SimpleStorage = artifacts.require("./SimpleStorage.sol");
 const Tether = artifacts.require("./Tether.sol");
 
 module.exports = async function (deployer) {
-  // deployer.deploy(SimpleStorage);
   await deployer.deploy(Tether);
 };
